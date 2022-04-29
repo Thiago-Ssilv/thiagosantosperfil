9a7f122f964c81acce603209ae7257fd39c8827d
thiago@thiagosantos <thiagossilva527@gmail.com>
2022-04-29 19:08:48 -0300

    adicionado arquivo hello.sh

---
+++ hello.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+let nome = "thiago santos"
+let idade = "23 anos"
+let funcao = "supervisor"
+ console.log(
+     `nome: ${nome}
+     idade ${idade}
+     funcao: ${funcao}`
+ )
+ 
+let meusalarioatual = 1400
+let salariopromocao = 800
+let meusalariopromovido = "meu salario e 2200"
+console.log (`meuslarioatual: ${meusalarioatual}
+salaraiopromocao: ${salariopromocao}`)
+console.log (meusalarioatual + salariopromocao)
+console.log (meusalariopromovido)
+exit;
